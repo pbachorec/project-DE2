f27a0e28e486a1482cba95d299ef2b6da3ef1745
dede-32 <165140571+dede-32@users.noreply.github.com>
2024-12-08 10:04:38 +0100

    Update acs712.c

--- DE2_Project_edit/lib/acs712/acs712.c
+++ DE2_Project_edit/lib/acs712/acs712.c
@@ -1,6 +1,5 @@
 #include "acs712.h"
 #include "adc.h"
-#include <util/delay.h> // Pro _delay_ms
 
 void acs712_init(void) {
     // Není nutné provádět další inicializaci, ADC je již nastaveno
@@ -14,7 +13,6 @@ float acs712_read_current(uint8_t channel, float sensitivity) {
         float voltage = (adc_value / 1023.0) * 5000.0; // Převod na napětí v mV
         float current = (voltage - 2500.0) / sensitivity; // Převod na proud v mA
         sum += current; // Přičtení aktuální hodnoty do sumy
-       // _delay_ms(10); // Krátká prodleva
     }
 
     return sum / 100; // Průměrování a vrácení výsledku
