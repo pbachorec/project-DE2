3c46b17bd3cf186fb9fbdc5220a1c9eeacf2c542
dede-32 <165140571+dede-32@users.noreply.github.com>
2024-12-08 10:01:53 +0100

    Update adc.h

--- DE2_Project_edit/lib/adc/adc.h
+++ DE2_Project_edit/lib/adc/adc.h
@@ -3,10 +3,10 @@
 
 #include <avr/io.h>
 
-// Inicializace ADC
+// Inicializace ADC (nastavení prescaleru a referenčního napětí)
 void adc_init(void);
 
-// Čtení hodnoty z ADC kanálu
+// Čtení hodnoty z daného ADC kanálu (0–7)
 uint16_t adc_read(uint8_t channel);
 
 #endif // ADC_H
