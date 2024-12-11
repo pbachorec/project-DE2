2c9278527fbba06286b4e218f6a5583faa050c60
Pidekk <165301715+Pidekk@users.noreply.github.com>
2024-12-11 14:04:13 +0100

    Update servo.c

--- DE2_Project_final/lib/servo/servo.c
+++ DE2_Project_final/lib/servo/servo.c
@@ -5,7 +5,7 @@ void servo_init(void) {
     TCCR1A |= (1 << WGM11) | (1 << COM1A1); // Fast PWM, non-inverting
     TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Prescaler 8
     ICR1 = 40000; // Set PWM period (20 ms for 50 Hz)
-    DDRB |= (1 << PB1); // Pin 9 (OC1A) jako výstup
+    DDRB |= (1 << PB1); // Pin 9 (OC1A) output
 }
 
 void servo_set_angle(uint8_t angle) {
