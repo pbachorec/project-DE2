85f17709da37194f4e16f3d30eba473a6e57bf01
Pidekk <165301715+Pidekk@users.noreply.github.com>
2024-12-11 13:51:24 +0100

    Update acs712.h

--- DE2_Project_final/lib/acs712/acs712.h
+++ DE2_Project_final/lib/acs712/acs712.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 
 // Initialize the ACS712 sensor
-void acs712_init(uint8_t channel, uint16_t sensitivity, uint16_t offset, uint16_t ref_voltage);
+void acs712_init(uint8_t channel, float sensitivity, float offset, float ref_voltage);
 
 // Measure current in amperes
-uint16_t acs712_read_current(void);
+float acs712_read_current(void);
 
 #endif // ACS712_H
